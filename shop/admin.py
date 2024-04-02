4b60f82cf9dc65023ce06a4b8db1f688df0bda33
MartialKing2023 <kuetchemartial1999@gmail.com>
2024-04-02 13:04:41 -0700

    Termine, mais avec une erreur mineure

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Commande
 
 # Register your models here.
+admin.site.site_header = "Site de E-Commerce || Toto-Shop"
+admin.site.site_title = "Toto-Shop"
+admin.site.index_title = "Manageur"
+
 class AdminCategory(admin.ModelAdmin):
     list_display = ('name', 'date_added')
     
 class AdminProduct(admin.ModelAdmin):
-    list_display = ('title', 'price', 'category', 'date_added')
+    list_display = ('title', 'price', 'category', 'date_added') # Afficher les elements ces details sous forme de tableau
+    search_fields = ('title',)   # Rechercher par rapport a
+    list_editable = ('price',)   # Les champs editables directement
+    
+class AdminCommande(admin.ModelAdmin):
+    list_display = ('items', 'nom', 'email', 'address', 'ville', 'pays', 'total', 'date_commande')
 
 admin.site.register(Product, AdminProduct)
 admin.site.register(Category, AdminCategory)
+admin.site.register(Commande, AdminCommande)
